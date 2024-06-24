c12a298949310734c434fe5b3ff1e7b1d19fd738
WebWithRathor <devrajrathor143@gmail.com>
2024-06-24 22:56:28 +0530

    Boiler Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainRoutes from './routes/MainRoutes'
+
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-cover overflow-hidden bg-center bg-[url(https://imgs.search.brave.com/Awk8292oa-EYzCMry-gLH8Y3cWDjt42RnrtWBPHr_bA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzA4LzQyLzQz/LzM2MF9GXzUwODQy/NDMzN19ZcXV4MTRW/MmQzVmZvWkpOSmc4/ZXBialpUQ2VNckJt/NS5qcGc)] min-h-screen h-full ">
+      <MainRoutes/>
+    </div>
+    </>
+  )
+}
+
+export default App
